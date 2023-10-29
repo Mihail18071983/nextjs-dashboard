0ba7219509861b1fd8af69750a1cba24f9333c95
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-10-29 14:56:53 +0200

    fix:handleSearch

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page',1)
+    params.set('page',"1")
     if (term) {
       params.set("query", term);
     } else {

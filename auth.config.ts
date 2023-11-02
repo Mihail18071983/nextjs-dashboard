dbd6aefef54e03d48e4f16ffbaacbbb1c34a3d79
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-11-02 11:33:45 +0200

    upd: add data to env. finish ch15

--- auth.config.ts
+++ auth.config.ts
@@ -7,17 +7,13 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log("user", auth?.user);
       const isLoggedIn = !!auth?.user;
-      console.log('is logged in:',isLoggedIn);
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      console.log('is on dashboard:',isOnDashboard);
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
-        
+        return Response.redirect(new URL("/dashboard", nextUrl)); 
       }
       return true;
     },
